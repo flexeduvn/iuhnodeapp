4772dd4271fcedad749c3a706f4f4e5ab3d6e828
hungphan <156576262+iuh20119051@users.noreply.github.com>
2024-03-28 15:26:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello! Student code is X');
+  res.send('Hello! Student code is 20119051');
 });
 
 app.listen(PORT, HOST);
